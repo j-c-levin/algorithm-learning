937f6c44fd89f6ceae186ef224873b4e76dff7b1
Joshua Levin <joshua.c.levin@gmail.com>
2018-03-03 12:54:12 +0000

    start mergesort

--- dist/merge_sort/index.js
+++ dist/merge_sort/index.js
@@ -1,7 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function mergeSort(input) {
+    if (input.length === 1) {
+        return input;
+    }
     return input;
 }
 exports.mergeSort = mergeSort;
+function merge(inputA, inputB) {
+    var output = [];
+    while (inputA.length > 0 && inputB.length > 0) {
+        var temp = inputA[0] > inputB[0] ? inputB.splice(0, 1) : inputA.splice(0, 1);
+        output.push(temp);
+    }
+    return output;
+}
 //# sourceMappingURL=index.js.map
--- src/sorting/merge_sort/index.ts
+++ src/sorting/merge_sort/index.ts
@@ -1,3 +1,16 @@
 export function mergeSort(input: Number[]): Number[] {
+    if (input.length === 1) {
+        return input;
+    }
   return input;
+}
+
+function merge(inputA: Number[], inputB: Number[]): Number[] {
+  const output = [];
+  while (inputA.length > 0 && inputB.length > 0) {
+    const temp =
+      inputA[0] > inputB[0] ? inputB.splice(0, 1) : inputA.splice(0, 1);
+    output.push(temp);
+  }
+  return output;
 }
